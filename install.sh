a9c5990aa1d01a012ee30f5b594f18bc48249ba9
Kellen Hart <kellenhart@gmail.com>
2019-02-07 22:46:05 -0800

    fix bugs w install

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 INSTALL_APP_DIR=$(dirname $_)/sys-install
 ADDITIONAL_INSTALLS="$@"
@@ -6,17 +5,21 @@ CODE_DIR="$HOME/dev"
 CONFIG_DIR="$HOME/.workstation-config"
 SETUP_DIR="$CONFIG_DIR/workstation-setup/setup"
 
-echo "which github user has access to applicable repos?"
-read $GITHUB_USER
+DEFAULT_USER=$(ssh -T git@github.com 2>&1 | grep -Eo ' (\S+)!' | sed 's/.$//' | sed 's/^.//')
+echo "If $DEFAULT_USER is correct github user, press enter."
+echo "... otherwise, enter current github user has access to applicable repos, or user you want to use on this computer"
+read GITHUB_USER
 
-if ! ssh -T git@github.com | grep $GITHUB_USER; then
-    ssh-keygen -t rsa -b 4096 -C $GITHUB_USER
-    cat ~/.ssh/id_rsa.pub
+if ! test -z $GITHUB_USER; then
+    if ! ssh -T git@github.com 2>&1 | grep 'Hi $GITHUB_USER!'; then
+        ssh-keygen -t rsa -b 4096 -C $GITHUB_USER
+        cat ~/.ssh/id_rsa.pub
 
-    echo "log into github with $GITHUB_USER and go to https://github.com/settings/keys,"
-    echo "add ssh key from above output"
-    echo "press return when done"
-    read
+        echo "log into github with $GITHUB_USER and go to https://github.com/settings/keys,"
+        echo "add ssh key from above output"
+        echo "press return when done"
+        read
+    fi
 fi
 
 mkdir -p $CONFIG_DIR
@@ -37,8 +40,19 @@ bootstrap_installation_environment $OS_FLAVOR
 echo "Which installation manifest would you like to use? "
 echo "Any github repo with a valid manifest.yaml"
 echo "Examples:"
-echo "kdavh/manifest-home"
-echo "kdavh/twilio-setup"
-read $MANIFEST_REPO
+echo "1. kdavh/manifest-home"
+echo "2. kdavh/twilio-setup"
+echo "or enter your own"
+read MANIFEST_REPO
 
+if test $MANIFEST_REPO = '1'; then
+    MANIFEST_REPO='kdavh/manifest-home'
+elif test $MANIFEST_REPO = '2'; then
+    MANIFEST_REPO='kdavh/twilio-setup'
+fi
+
+which python
+python -V
+pwd
+ls
 python ./install.py $OS_FLAVOR $MANIFEST_REPO
--- setup/installation_helpers.sh
+++ setup/installation_helpers.sh
@@ -8,13 +8,14 @@ function bootstrap_installation_environment() {
 
     if [ ! -d ./venv ]; then
         if [ "$os_flavor" = 'debian' ]; then
-		sudo apt install -y python3-pip;
-	elif [ "$os_flavor" = 'mac' ]; then
-		if [ ! command -v brew ]; then
-			/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-			brew install -y python3
-		fi
-	else
+		    sudo apt install -y python3-pip;
+	    elif [ "$os_flavor" = 'mac' ]; then
+            if [ ! command -v brew ]; then
+                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            fi
+            brew install -y python3
+        fi
+
 		echo "OS FLAVOR \"${os_flavor}\"" not handled
 		exit 1
         fi
