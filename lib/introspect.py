2aa48647f5e4ccc1da6febebc2fd91350c4a26f5
kdavh <kellenhart@gmail.com>
2019-02-12 23:00:20 -0800

    Update introspect.py
    more python 3.5 compat

--- lib/introspect.py
+++ lib/introspect.py
@@ -1,10 +1,12 @@
-from subprocess import run
+from subprocess import run, PIPE
 
 from __init__ import LIB_DIR
 
 def os_flavor():
     return run(
         "source {}/installation_helpers.sh; os_flavor".format(LIB_DIR),
-        capture_output=True,
+        # capture_output=True only available in python 3.7
+        stdout=PIPE,
+        stderr=PIPE,
         shell=True
     ).stdout.decode().strip()
