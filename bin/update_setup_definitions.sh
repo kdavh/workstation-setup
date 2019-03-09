98d257afe28d1da0564d03f3a54d264a30a78de0
Kellen Hart <kellenhart@gmail.com>
2019-03-09 23:08:43 +0000

    Update bin/update_setup_definitions.sh

--- bin/update_setup_definitions.sh
+++ bin/update_setup_definitions.sh
@@ -8,17 +8,25 @@ echo "\nUpdates package_definitions, manifests, and workstation-setup repo\n\n"
 
 function rebase_and_push() {
     cd $1
-    echo "processing $1"
+    notify_highlight "Processing $1"
     git add .
+    if test -z "$(git diff-index --name-only HEAD --)"; then
+        echo "$1 has no working changes, rebasing and pushing if needed"
+        git pull --rebase
+        git push
+        return 0
+    fi
     git diff HEAD
     git status
-    notify_highlight "\nCommit and push changes?"
-    read yn
+    notify_highlight "\nCommit and push changes? Enter commit message, 'y' for default message, or nothing to skip."
+    read msg
 
-    if [ "$yn" == 'y' ]; then
-        notify_highlight "\nWhat is the commit message?"
-        read msg
+    if test -n "$msg"; then
+        if [ "$msg" == 'y' ]; then
+            msg="Update $(git diff-index --name-only HEAD -- | paste -sd " " -)"
+        fi
         git commit -m "$msg"
+        git pull --rebase
         git push
     fi;
 }
